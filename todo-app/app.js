6b735a4453865815e4320365e96783796fc18678
fujiiiiiiiii <fko2447019@stu.o-hara.ac.jp>
2025-06-03 14:53:45 +0900

    <link rel=stylesheet href=style.css>追加

--- todo-app/app.js
+++ todo-app/app.js
@@ -1,31 +1,38 @@
-// jsから入力欄にアクセスできるようになる
 const taskInput = document.getElementById('taskInput');
-// id="addTaskBtn"の<button>要素を取得
 const addTaskBtn = document.getElementById('addTaskBtn');
-// タスク一覧を表示する<ul id="taskList">"要素を取得
 const taskList = document.getElementById('taskList');
-// 追加ボタン学理kkされたら中の処理を実行
+
 addTaskBtn.addEventListener('click', () => {
-    // 入力欄に書かれた百地をtaskTextに代入
-    const taskText = taskInput.value;
-    // もし何も入力されなければ処理を中断
+    const taskText = taskInput.value.trim();
     if (taskText === '') return;
 
-    // 新しい<li>を作って、入力されたテキストを入れる
     const li = document.createElement('li');
-    li.textContent = taskText;
+
+    // タスク名（スパンで囲む）
+    const taskTextSpan = document.createElement('span');
+    taskTextSpan.textContent = taskText;
+    taskTextSpan.style.margin = '0 8px';
+
+    // 完了チェックボックス
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox'
+    checkbox.addEventListener('click', () => {
+        if (checkbox.checked) {
+            taskTextSpan.style.textDecoration = 'line-through';
+        } else {
+            taskTextSpan.style.textDecoration = 'none';
+        }
+    });
 
     // 削除ボタン
-    // <button>要素を作り、表示文字を削除に設定
     const deleteBtn = document.createElement('button');
     deleteBtn.textContent = '削除'
-    // 削除ボタンがクリックされたら、対応する<li>を削除
     deleteBtn.addEventListener('click', () => {
         li.remove();
     });
     
-    // 作ったボタンを<li>に追加し、それをulに追加
-    // これで画面にタスクが表示される
+    li.appendChild(checkbox);
+    li.appendChild(taskTextSpan);
     li.appendChild(deleteBtn);
     taskList.appendChild(li);
 
