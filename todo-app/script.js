70411e9191a44ce68738485a0d90194caf7e68d9
fujiiiiiiiii <fko2447019@stu.o-hara.ac.jp>
2025-06-20 10:29:18 +0900

    タスクチェック機能、削除機能

--- todo-app/script.js
+++ todo-app/script.js
@@ -16,9 +16,27 @@ function addTodo() {
     const li = document.createElement('li')
     li.textContent = text;
 
+    // クリックで完了状態を切り替える(取り消し線)
+    li.addEventListener('click', () => {
+        li.classList.toggle('completed');
+    });
+
+    // 削除ボタン作成
+    const delBtn = document.createElement('button');
+    delBtn.textContent = '削除';
+    delBtn.addEventListener('click', (event) => {
+        event.stopPropagation(); // liのクリックイベントを防ぐ
+        todoList.removeChild(li);
+    });
+
+    li.appendChild(delBtn);
+    todoList.appendChild(li);
+
+
     // リストに追加
     todoList.appendChild(li);
 
+    //入力欄リセット
     input.value = '';
 };
 
