b6ec83f0565ee25bfb2998ec7e252bd758ca32d3
fujiiiiiiiii <fko2447019@stu.o-hara.ac.jp>
2025-06-20 10:48:58 +0900

    AJAXでJSONファイルから初期TODOを読み込む

--- todo-app/script.js
+++ todo-app/script.js
@@ -2,43 +2,83 @@ const input = document.getElementById('todoInput');
 const addBtn = document.getElementById('addBtn');
 const todoList = document.getElementById('todoList');
 
-//ToDOを追加する関数
-function addTodo() {
-    const text = input.value.trim(); // 入力値を取得(前後の空白を削除)
+async function loadTodos() {
+    try{
+        const response = await fetch('todos.json'); // JSONファイル取得
+        if (!response.ok) {
+            throw new Error('ネットワークエラー');
+        }
+        const todos = await response.json();
 
-    // 入力が空ならアラートを出して終了
-    if (text === '') {
-        alert('ToDoを入力してください')
-        return;
+        todos.ForEach(todo => {
+            addTodoFromText(todo.text);
+        });
+    }catch (error) {
+        alert('ToDoの読み込みに失敗しました: ' + error.message);
     }
+}
 
-    // <li>要素を作って内容をセット
-    const li = document.createElement('li')
+function addTodoFromText(text) {
+    const li = document.createElement('li');
     li.textContent = text;
 
-    // クリックで完了状態を切り替える(取り消し線)
     li.addEventListener('click', () => {
         li.classList.toggle('completed');
     });
 
-    // 削除ボタン作成
     const delBtn = document.createElement('button');
     delBtn.textContent = '削除';
     delBtn.addEventListener('click', (event) => {
-        event.stopPropagation(); // liのクリックイベントを防ぐ
+        event.stopPropagation();
         todoList.removeChild(li);
     });
 
     li.appendChild(delBtn);
     todoList.appendChild(li);
+}
+
+//ページ読み込み時に呼び出す
+window.addEventListener('DOMContentLoaded', loadTodos);
 
 
-    // リストに追加
-    todoList.appendChild(li);
 
-    //入力欄リセット
-    input.value = '';
-};
+// //ToDOを追加する関数
+// function addTodo() {
+//     const text = input.value.trim(); // 入力値を取得(前後の空白を削除)
 
-// ボタンをクリックしたらaddTodo関数を呼び出す
-addBtn.addEventListener('click', addTodo);
+//     // 入力が空ならアラートを出して終了
+//     if (text === '') {
+//         alert('ToDoを入力してください')
+//         return;
+//     }
+
+//     // <li>要素を作って内容をセット
+//     const li = document.createElement('li')
+//     li.textContent = text;
+
+//     // クリックで完了状態を切り替える(取り消し線)
+//     li.addEventListener('click', () => {
+//         li.classList.toggle('completed');
+//     });
+
+//     // 削除ボタン作成
+//     const delBtn = document.createElement('button');
+//     delBtn.textContent = '削除';
+//     delBtn.addEventListener('click', (event) => {
+//         event.stopPropagation(); // liのクリックイベントを防ぐ
+//         todoList.removeChild(li);
+//     });
+
+//     li.appendChild(delBtn);
+//     todoList.appendChild(li);
+
+
+//     // リストに追加
+//     todoList.appendChild(li);
+
+//     //入力欄リセット
+//     input.value = '';
+// };
+
+// // ボタンをクリックしたらaddTodo関数を呼び出す
+// addBtn.addEventListener('click', addTodo);
